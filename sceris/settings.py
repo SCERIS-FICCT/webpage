5f6af1a84323c24731c4262f17594f89cd77cf15
Mauricio Ballesteros <mballes95@gmail.com>
2020-04-12 16:12:13 -0400

    Adding Apps

--- sceris/settings.py
+++ sceris/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'academic.apps.AcademicConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
