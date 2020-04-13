96e77a9beb2571c3390e12848f30ad28694f0b25
Mauricio Ballesteros <mballes95@gmail.com>
2020-04-12 23:11:54 -0400

    Config Template Done

--- sceris/urls.py
+++ sceris/urls.py
@@ -17,10 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
